021df780075b7a9e3a146430526cab5d39475643
x-h168 <87917751+x-h168@users.noreply.github.com>
2023-07-06 16:41:25 +0800

    Update mtd_storage.sh

--- trunk/user/scripts/mtd_storage.sh
+++ trunk/user/scripts/mtd_storage.sh
@@ -3,7 +3,7 @@
 result=0
 mtd_part_name="Storage"
 mtd_part_dev="/dev/mtdblock5"
-mtd_part_size=196608
+mtd_part_size=1024000
 dir_storage="/etc/storage"
 slk="/tmp/.storage_locked"
 tmp="/tmp/storage.tar"
